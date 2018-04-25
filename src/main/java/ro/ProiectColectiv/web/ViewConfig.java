e333d972a4647be3ba700e94e6f9aa4a3e270fe3
adriandan25 <adriandan1994@gmail.com>
2018-04-25 17:31:01 +0300

    Mapping

---
+++ src/main/java/ro/ProiectColectiv/web/ViewConfig.java
@@ -0,0 +1,38 @@
+package ro.ProiectColectiv.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@Configuration
+public class ViewConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    public ViewConfig()
+    {
+    }
+
+    public void addViewControllers(ViewControllerRegistry registry)
+    {
+        registry.addViewController( "/doctor").setViewName("Doctor");
+        //registry.addViewController( "/doctor").setViewName("Doctor");
+        //registry.addViewController(urlPath: "/").setViewName("index");
+    }
+
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry)
+    {
+        resourceHandlerRegistry.addResourceHandler("/resources/FrontEnd/**").addResourceLocations("classpath:/resources/FrontEnd/");
+    }
+
+}
